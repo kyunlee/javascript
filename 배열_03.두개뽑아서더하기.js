8c91eab52b6d3afb773c77365665c1e369b6867f
kyunlee <kyunlee87@gmail.com>
2026-06-11 17:16:00 +0900

    배열3

--- 배열_03.두개뽑아서더하기.js
+++ 배열_03.두개뽑아서더하기.js
@@ -1,5 +1,15 @@
+
 function solution(arr){
+    var answer = [];
+    for(let i=0; i < arr.length ; i++ ){
+        for(let j=0; j <i; j++){
+            answer.push(arr[i]+arr[j]);
+            //console.log("answer ="+ i+" "+j+" " + answer);
+        }
+    }
     
+    return [...new Set(answer)].sort((a,b)=> a-b);
 }
 
-cosole.log(solution([2,1,3,4,1]));
+console.log(solution([2,1,3,4,1])); //[2,3,4,5,6,7];
+console.log(solution([5,0,2,7]));   //[2,5,7,9,12];
