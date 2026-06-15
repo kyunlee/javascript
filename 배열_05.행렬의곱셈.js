636be68b89e3dc6d1c95bf75bacaf4282450231c
kyunlee <kyunlee87@gmail.com>
2026-06-15 17:20:25 +0900

    배열6

--- 배열_05.행렬의곱셈.js
+++ 배열_05.행렬의곱셈.js
@@ -4,7 +4,7 @@
 //            [3,2] ,                 [3,3] ]
 //            [4,1] ] 
 //    
-/*
+
 function solution(arr1, arr2) {
     let answer = [];
 
@@ -32,27 +32,7 @@ function solution(arr1, arr2) {
     
     return answer;
 }
-*/
 
-function solution(arr1, arr2) {
-    let answer =[];
-
-    for(let i=0; i < arr1.length; i++){
-        let row =[];
-
-        for(let j=0; j<arr2[0].length; j++){
-            let sum = 0;
-            
-            for(let k=0; k<arr1[0].length; k++){
-                sum += arr1[i][k] * arr2[k][j];
-            }
-            row.push(sum);
-        }
-        answer.push(row);
-    }
-
-    return answer;
-}
 const arr1 = [[1, 4], [3, 2], [4, 1]];
 const arr2 = [[3, 3], [3, 3]];
 
