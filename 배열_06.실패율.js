ec1b038db346ce6f85a6389965080495f6e2725e
kyunlee <kyunlee87@gmail.com>
2026-06-15 21:15:53 +0900

    배열6

---
+++ 배열_06.실패율.js
@@ -0,0 +1,22 @@
+// 실패율
+
+function solution(N, stages){
+    const temparr = new Array(N+2).fill(0);
+    console.log(temparr);
+    
+    for(const stage of stages){
+
+        temparr[stage]++;
+    }
+    console.log(temparr);
+    // 정리가 끝난 상태: [0, 1, 3, 2, 1, 0, 1]
+
+}
+
+//입출력의 예
+// N    stages                result
+// 5   [2,1,2,6,2,4,3,3]     [3,4,3,2,5]
+//console.log(solution(5, [2,1,2,6,2,4,3,3]));
+
+// 테스트 해보기
+console.log(solution(5, [2, 1, 2, 6, 2, 4, 3, 3])); // 결과: [3, 4, 2, 1, 5]
