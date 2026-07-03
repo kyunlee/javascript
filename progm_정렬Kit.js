245a9df9fed2095013788384f64c98e4180eb70e
kyunlee <kyunlee87@gmail.com>
2026-07-03 12:09:04 +0900

    정렬1

--- progm_정렬Kit.js
+++ progm_정렬Kit.js
@@ -1,5 +1,5 @@
 //K번째수
-
+/* 내가 푼 소스
 function solution(array, commands) {
     var answer = [];
     var temp = [];
@@ -15,5 +15,17 @@ function solution(array, commands) {
     
     return answer;
 }
+*/
+// AI
+function solution(array, commands) {
+
+    return commands.map(([i,j,k])=>{
+        
+        return array.slice(i-1,j).sort((a,b)=>a-b)[k-1];
+
+    });
+
+}
+
 
 console.log(solution([1,5,2,6,3,7,4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]));
