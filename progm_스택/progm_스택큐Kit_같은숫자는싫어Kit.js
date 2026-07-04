eee3cace6c952aff97e561ef8e61d26faf3cdf75
kyunlee <kyunlee87@gmail.com>
2026-07-04 14:43:46 +0900

    고득점Kit

--- progm_스택/progm_스택큐Kit_같은숫자는싫어Kit.js
+++ progm_스택/progm_스택큐Kit_같은숫자는싫어Kit.js
@@ -1,9 +1,6 @@
 // 전체 중복 제거
 // [...new Set(arr)] (예: [1, 2, 1] -> [1, 2])
-
-// 스택/큐> 같은 숫자는 싫어
-// 연속 중복 제거
-
+/*
 function solution(arr)
 {
     
@@ -11,4 +8,15 @@ function solution(arr)
 }
 
 
-console.log(solution([3, 0, 6, 1, 5]));
+console.log(solution([1,1,3,3,0,1,1]));
+*/
+
+// 스택/큐> 같은 숫자는 싫어
+// 연속 중복 제거
+
+function solution(arr)
+{
+    return arr.filter((a,index)=> a != arr[index-1] );
+}
+
+console.log(solution([1,1,3,3,0,1,1]));
