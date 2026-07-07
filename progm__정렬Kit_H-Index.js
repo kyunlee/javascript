7e9dc3d951fc7f53bbd0b2874708cd1476020ba9
kyunlee <kyunlee87@gmail.com>
2026-07-07 15:24:04 +0900

    문자열

--- progm__정렬Kit_H-Index.js
+++ progm__정렬Kit_H-Index.js
@@ -1,11 +1,13 @@
 function solution(citations) {
 
     citations.sort((a,b)=> b-a);
-
+    console.log(citations);
     var answer = 0;
 
     for(let i=0; i<citations.length; i++){
 
+        console.log(citations[i]+">"+i);
+        
         if(citations[i]>= i+1){
             answer++;
         }else{
--- progm_문자열.js
+++ progm_문자열.js
@@ -1,9 +1,11 @@
-// 문자열이 몇 번 등장하는지 세기
+/**
+ * 1.문자열이 몇 번 등장하는지 세기
+ */
 
 //중첩된 경우도 포함하여 계산해야 하는지, 
 //겹치지 않게 계산해야 하는지에 따라 접근 방식이 달라짐
 
-//1. indexOf와 반복문을 사용하는 방식 (가장 권장) => 중첩된 경우 포함
+//1-1) indexOf와 반복문을 사용하는 방식 (가장 권장) => 중첩된 경우 포함
 
 function solution(myString, pat){
     let count = 0;
@@ -20,10 +22,22 @@ function solution(myString, pat){
 
 console.log(solution("banana","ana"));
 
-//2. split을 활용하는 방식 (매우간결) =>중첩된 패턴 카운드 못함
+//1-2) split을 활용하는 방식 (매우간결) =>중첩된 패턴 카운드 못함
 function solution2(myString, pat){
     console.log(myString.split(pat));
     return myString.split(pat).length-1;
 }
 
 console.log(solution2("banana","ana"));
+/***
+ * 2. 정규식 표현 
+ */
+
+//2-1) 세개의 
+
+function solution3(myStr) {
+    var answer = myStr.split(/[abc]+/).filter((str)=> str!="");
+    return answer.length == 0 ? ["EMPTY"] : answer;
+}
+
+console.log(solution3("baconlettucetomato"));
